0dc3c958eeeaf71e6a5ccf269acb61072a1961bf
ivana-antunovic <kian.obadic@gmail.com>
2025-10-21 12:36:32 +0200

    Update Projectile.cs

--- VisionsForetold/Assets/_Scripts/Game/Projectile.cs
+++ VisionsForetold/Assets/_Scripts/Game/Projectile.cs
@@ -13,7 +13,7 @@ public class Projectile : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 
     private void OnTriggerEnter(Collider other)
